fde2cb551ca7bc8418b7ab6caef02352598dce33
Student <student@iMac-Astrum-30.local>
2024-12-23 17:20:48 +0500

    homework finished

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=20)
+    product_description = models.CharField(max_length=20)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_image = models.ImageField(upload_to='product_images/')
+    product_category = models.CharField(max_length=100)
+
+
